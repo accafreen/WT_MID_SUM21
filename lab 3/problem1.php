bae43661854698d31fbbe2567a553531efb040f2
afreen hasan priyanka <afreenpriyanka50@gmail.com>
2021-06-18 22:04:28 +0600

    lab3

---
+++ lab 3/problem1.php
@@ -0,0 +1,18 @@
+
+<html>
+  <head>
+    <title>problem1</title>
+  </head>
+  <body>
+    <span>the quick brown</span>
+    <?php
+echo "<b>fox</b>";
+    ?>
+    <span>jumps over a lazy</span>
+    <?php
+echo "<b>dog</b>";
+    ?>
+
+
+  </body>
+</html>
